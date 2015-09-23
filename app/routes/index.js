223ce9c48398d79cfcbe3472f46f5406dbe8d674
Carlo DeGregorio <cdegregorio@gmail.com>
2015-09-23 11:07:45 -0700

    add initial files. Index, contact, about routes created

---
+++ app/routes/index.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+var posts = [{
+  id: 1,
+  user: "Jon Parsons",
+  title: "Where I've been...",
+  blogPost: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}, {
+  id: 2,
+  user: "Peter Piper",
+  title: "Where I am...",
+  blogPost: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}, {
+  id: 3,
+  user: "J Sands",
+  title: "Where I am going...",
+  blogPost: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}];
+
+export default Ember.Route.extend({
+  model() {
+    return posts;
+  }
+});
