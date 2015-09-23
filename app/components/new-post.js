f7190d6590bfff2c4bb9aa17b6abe6f1a6ce8591
Carlo DeGregorio <cdegregorio@gmail.com>
2015-09-23 16:39:41 -0700

    added a form to add posts to record

---
+++ app/components/new-post.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+    postFormShow() {
+      this.set('addNewPost', true);
+    }
+  }
+});
