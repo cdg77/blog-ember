6bd7518a107d95a3651d9e568546c31f88ae334e
Carlo DeGregorio <cdegregorio@gmail.com>
2015-09-23 12:13:00 -0700

    added firesbase and updated the model hook

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,27 +1,7 @@
 import Ember from 'ember';
 
-var posts = [{
-  id: 1,
-  user: "Jon Parsons",
-  title: "Where I've been...",
-  blogPost: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  image: "#"
-}, {
-  id: 2,
-  user: "Frosty D. Snowman",
-  title: "Chillin'... Halloween",
-  blogPost: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  image: "http://vignette4.wikia.nocookie.net/p__/images/1/12/Frosty_the_Snowman_creative_card.png/revision/latest?cb=20140721201014&path-prefix=protagonist"
-}, {
-  id: 3,
-  user: "J Sands",
-  title: "Where I am going...",
-  blogPost: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  image: "#"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return posts;
+    return this.store.findAll('post');
   }
 });
