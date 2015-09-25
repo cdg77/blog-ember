432ed9a2b4ea3eb943332141c41608795121c3d1
cdg77 <cdegregorio@gmail.com>
2015-09-24 22:46:16 -0700

    update a blog post

--- app/components/update-post.js
+++ app/components/update-post.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updatePostForm: false,
+  actions: {
+    updatePostForm() {
+      this.set('updatePostForm', true);
+    },
+    update(post) {
+      var params = {
+        user: this.get('user'),
+        title: this.get('title'),
+        image: this.get('image'),
+        blogPost: this.get('blogPost')
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('update', post, params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -14,7 +14,11 @@ export default Ember.Route.extend({
     },
 
     update(post, params) {
-      // code to update will go here
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !== undefined) {
+          post.set(key, params[key]);
+        }
+      });
       post.save();
       this.transitionTo('index');
     },
